29c6b0dc8756c7fe8d511371c52788d3f736e07e
Olivier-rw <onshimiyimana17@alustudent.com>
2019-03-29 20:49:50 +0200

    Two pages

--- lib/about.dart
+++ lib/about.dart
@@ -1,75 +1,172 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'my_flutter_app_icons.dart';
 
 String title = "My Statement";
-String about = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Ut enim adminim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptatevelit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+String about =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Ut enim adminim veniam, quis nostrud exercitation ullamcolaboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptatevelit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 void main() => runApp(new About());
-
 
 class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      theme: ThemeData(fontFamily: 'Open Sans'),
       home: Scaffold(
-        drawer: Drawer(),
-        appBar: AppBar(
-           title: new Center(child: Text(title),),
-          backgroundColor: Colors.redAccent,
-
+        drawer: new Drawer(),
+        appBar: new AppBar(
+          leading: Builder(
+            builder: (context) => IconButton(
+                  icon: new Icon(Icons.apps),
+                  onPressed: () => Scaffold.of(context).openDrawer(),
+                ),
+          ),
+          title: new Center(
+            child: Text(
+              title,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          backgroundColor: Color(0xffEB6B5E),
         ),
-        
-        body: 
-        new Padding(padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0), child:
-        new Card( 
-          child: new Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              new Container(
-                padding: new EdgeInsets.all( 25.0),
-                child: new Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
+        body: ListView(children: <Widget>[
+          new Padding(
+            padding: EdgeInsets.fromLTRB(50.0, 0, 50.0, 0),
+            child: new Column(
+              children: <Widget>[
+                new Container(
+                  padding: new EdgeInsets.fromLTRB(0, 25.0, 0, 25.0),
+                  child: new Row(
+                    children: <Widget>[
+                      new Column(
+                        children: <Widget>[
+                          Text(
+                            "About Us",
+                            style: TextStyle(fontSize: 25.0),
+                          ),
+                          Text(
+                            "V.2019",
+                            style: new TextStyle(fontSize: 17.5),
+                          )
+                        ],
+                      ),
+                      // new Image Logo;
+                      Padding(
+                        padding: EdgeInsets.only(left: 150),
+                        child: Image.asset(
+                          'assets/logo.png',
+                          height: 50.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                new Text(
+                  about,
+                  style: new TextStyle(fontSize: 14),
+                ),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(0, 20.0, 0, 20.0),
+                    child: Text(
+                      "You can contact us via these means",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    )),
+                Row(
                   children: <Widget>[
-                    new Column(children: <Widget>[
-                      Text("About Us", style: TextStyle(fontSize: 25.0),), 
-                      Text("V.2019", style: new TextStyle(fontSize: 17.5),)],),
-                    // new Image Logo;
-                    
-                    
+                    // Icon for mail;
+                    IconButton(
+                      icon: Icon(MyFlutterApp.mail_outline),
+                      onPressed: () {},
+                    ),
+                    Text("hello@company.com"),
                   ],
                 ),
-              ),Center(child:new Text(about, style: new TextStyle(fontSize: 16),),),
+                Row(
+                  children: <Widget>[
+                    // Icon for phone
+                    IconButton(
+                      icon: Icon(MyFlutterApp.smartphone),
+                      onPressed: () {},
+                    ),
+                    Text("+250 889 8953")
+                  ],
+                ),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                    child: Text("Say hello to us on social media")),
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(MyFlutterApp.twitter_circled),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(MyFlutterApp.facebook_circled),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                    child: Text("Send Us Feedback")),
 
-              Padding(padding: EdgeInsets.fromLTRB(0, 20.0, 0 , 20.0), 
-              child:Text("You can contact us via these means", style: TextStyle(fontSize: 20.0),)),
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Full Name",
+                      border: InputBorder.none,
+                      fillColor: (Color(0xffFAFAFA))),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                  child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Email Address",
+                          border: InputBorder.none,
+                          fillColor: Color(0xffFAFAFA))),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                  child: TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Phone",
+                          border: InputBorder.none,
+                          fillColor: Color(0xffFAFAFA))),
+                ),
+                // Text Area
+                Container(
+                  height: 200,
+                  decoration: BoxDecoration(color: Color(0xffFAFAFA)),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Message",
+                      border: InputBorder.none,
+                    ),
+                    maxLines: null,
+                  ),
+                ),
 
-              Row(children: <Widget>[
-                // Icon for mail;
-                Text("hello@company.com"),
-              ],),
-              Row(children: <Widget>[
-                // Icon for phone
-                Text("+250 889 8953")
-              ],)
-             
-            ],
+                Align(
+                  alignment: FractionalOffset(1, 0),
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
+                      child: Text(
+                        "SUBMIT",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    color: Color(0xffEB6B5E),
+                  ),
+                )
+              ],
+            ),
           ),
-          color: Colors.white,
-          
-        ),
-        
-
-//        body: new Padding(  padding: EdgeInsets.all(25.0),  child: const Card(
-//          child: Center(child:
-//          Text( "View some sample photos of what your building could look like", style: TextStyle( fontSize: 15.0, ),),
-//          ),
-//)
-//        ),
-      
+        ]),
+        backgroundColor: Colors.white,
       ),
-      backgroundColor: Colors.white,
-      ),
-      
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,68 +2,97 @@ import 'package:flutter/material.dart';
 import 'about.dart';
 
 String title = "My Statement";
-String topText = "View some sample photos of what your building could look like";
+String topText =
+    "View some sample photos of what your building could look like";
 
-void main() => runApp(new About());
-
+void main() => runApp(new App());
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      theme: ThemeData(fontFamily: 'Open Sans'),
       home: Scaffold(
-        drawer: Drawer(), 
+        drawer: Drawer(),
         appBar: AppBar(
-           title: new Center(child: Center(child: Text(title),),),
-          backgroundColor: Colors.redAccent,
+          leading: Builder(
+            builder: (context) => IconButton(
+                  icon: new Icon(Icons.apps),
+                  onPressed: () => Scaffold.of(context).openDrawer(),
+                ),
+          ),
+          title: Center(
+              child: Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          )),
+          backgroundColor: Color(0xffEB6B5E),
         ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(50.0, 25.0, 50.0, 25.0),
+          child: ListView(children: [
+            new Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    topText,
+                    style: TextStyle(
+                      fontFamily: 'Open Sans',
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
 
-        body:  new Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              new Padding(  padding: EdgeInsets.all(25.0),  child: const Card(
-              child: Center(child:
-              Text( "View some sample photos of what your building could look like", style: TextStyle( fontSize: 15.0, ),),
-              ),
-              // new Container(
-              //   padding: new EdgeInsets.all( 25.0),
-              //   child: new Row(
-              //     crossAxisAlignment: CrossAxisAlignment.start,
-              //     mainAxisAlignment: MainAxisAlignment.center,
-              //     children: <Widget>[
-              //       new Text(
-              //           '\$  ',
-              //           style: new TextStyle(
-              //             fontSize: 20.0,
-              //             fontFamily: 'Roboto',
-              //             color: new Color(0xFF26C6DA),
-              //           )
-              //       ),
-              //       new Text(
-              //         '3,435.23',
-              //         style: new TextStyle(
-              //             fontSize: 35.0,
-              //             fontFamily: 'Roboto',
-              //             color: new Color(0xFF26C6DA)
-              //         ),
-              //       )
-              //     ],
-              //   ),
-              // ),
-              // new Text('general balance'),
-              ),)],
-          ), 
-        
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.black)),
+                  padding: EdgeInsets.fromLTRB(62.5, 0, 62.5, 0),
+                  child: Image.asset(
+                    'assets/logo.png',
+                    height: 175.0,
+                  ),
+                ),
 
-//        body: new Padding(  padding: EdgeInsets.all(25.0),  child: const Card(
-//          child: Center(child:
-//          Text( "View some sample photos of what your building could look like", style: TextStyle( fontSize: 15.0, ),),
-//          ),
-//)
-//        ),
-      
+                // Padding()
+                new Padding(
+                  padding: EdgeInsets.only(top: 15.0),
+                  child: Card(
+                    child: Container(
+                      padding: EdgeInsets.only(left: 5),
+                      height: 250,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 1, color: Colors.red)),
+                      child: TextField(
+                          maxLines: null,
+                          decoration: InputDecoration(border: InputBorder.none
+                              // border: OutlineInputBorder(
+                              //     borderSide: BorderSide(color: Colors.red)),
+                              ),
+                          style: TextStyle(
+                              // height: 10,
+                              fontSize: 14,
+                              color: Colors.black)),
+                    ),
+                  ),
+                ),
+
+                Align(
+                  alignment: FractionalOffset(1, 0.6),
+                  child: Container(
+                    child: new RaisedButton(
+                      onPressed: () {},
+                      child: Text("DOWNLOAD"),
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ]),
+        ),
       ),
-      
     );
   }
 }
