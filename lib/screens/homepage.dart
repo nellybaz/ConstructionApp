0ef2187bd689b4af6e5fdb16f605e72fafbe0c9d
Nelson Bassey <nellybaz10@gmail.com>
2019-04-03 16:23:52 +0200

    i dont know

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import './my_flutter_app_icons.dart';
+import './questionnaire.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: Text(
+            "Home Page",
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(MyFlutterApp.th, color: Colors.white),
+          ),
+        ),
+        body: ListView(children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(
+                    top: 140.0, left: 50.0, right: 50.0, bottom: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            margin: EdgeInsets.only(right: 15.0, bottom: 10.0),
+                            height: 160.0,
+                            decoration: BoxDecoration(color: Colors.redAccent),
+                            child: FlatButton(
+                              onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => Questionnaire()));
+                                print("Questionnaire Pressed");
+                              },
+                              child: Container(
+                                margin: EdgeInsets.only(top: 40.0),
+                                child: Column(
+                                  children: <Widget>[
+                                    Center(
+                                      child: IconButton(
+                                        onPressed: (){},
+                                        icon: Icon(
+                                          FontAwesomeIcons.question,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                    Center(
+                                      child: Text(
+                                        "Questionnaire",
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            margin: EdgeInsets.only(left: 15.0, bottom: 10.0),
+                            height: 160.0,
+                            decoration: BoxDecoration(color: Colors.redAccent),
+                            child: FlatButton(
+                              onPressed: () {
+                                print("My statement pressed");
+                              },
+                              child: Container(
+                                  margin: EdgeInsets.only(top: 40.0),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Center(
+                                        child: IconButton(
+                                          onPressed: (){},
+                                          icon: Icon(
+                                            FontAwesomeIcons.book,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      Center(
+                                        child: Text(
+                                          "My Statement",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      )
+                                    ],
+                                  )),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Container(
+                            margin: EdgeInsets.only(right: 15.0, top: 70.0),
+                            height: 160.0,
+                            decoration: BoxDecoration(color: Colors.redAccent),
+                            child: FlatButton(
+                              onPressed: () {
+                                print("Call Pressed");
+                              },
+                              child: Container(
+                                  margin: EdgeInsets.only(top: 40.0),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Center(
+                                        child: IconButton(
+                                          onPressed: (){},
+                                          icon: Icon(
+                                            FontAwesomeIcons.phone,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      Center(
+                                        child: Text(
+                                          "Call an Agent",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      )
+                                    ],
+                                  )),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            margin: EdgeInsets.only(left: 15.0, top: 70.0),
+                            height: 160.0,
+                            decoration: BoxDecoration(color: Colors.redAccent),
+                            child: FlatButton(
+                              onPressed: () {
+                                print("Contact pressed");
+                              },
+                              child: Container(
+                                  margin: EdgeInsets.only(top: 40.0),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Center(
+                                        child: IconButton(
+                                          onPressed: (){},
+                                          icon: Icon(
+                                            FontAwesomeIcons.user,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      Center(
+                                        child: Text(
+                                          "Contact Us",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      )
+                                    ],
+                                  )),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 130.0),
+                child: FlatButton(
+                    padding: EdgeInsets.all(20.0),
+                    // color: Colors.white,
+                    splashColor: Colors.tealAccent,
+                    onPressed: () {
+                      print("You have been signed out");
+                    },
+                    child: Text(
+                      "Sign Out",
+                      style: TextStyle(color: Colors.redAccent),
+                    )),
+              )
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
---
+++ pages5to8/lib/inputpage.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'UserInput.dart';
+import './my_flutter_app_icons.dart';
+
+class InputPage extends StatelessWidget{
+  @override
+
+  Widget build (BuildContext context){
+    return Scaffold(
+        body: ListView(
+                  children:<Widget>[ Container(
+            margin:
+                EdgeInsets.only(bottom: 20.0, left: 50.0, right: 50.0, top: 70.0),
+            child: Column(
+              children: <Widget>[
+                UserInput(
+                    "FULL NAME",
+                    "Joseph Mamady",
+                    false,
+                    Icon(
+                      Icons.supervised_user_circle,
+                      color: Colors.black87,
+                    )),
+                UserInput(
+                    "PHONE NUMBER",
+                    "+250 783 820 538",
+                    false,
+                    Icon(
+                      Icons.phone_android,
+                      color: Colors.black87,
+                    )),
+                UserInput("EMAIL", "jomamady@example.com", false,
+                    Icon(Icons.email, color: Colors.black87)),
+                UserInput(
+                    "PASSWORD",
+                    ".......",
+                    true,
+                    Icon(
+                      Icons.security,
+                      color: Colors.black87,
+                    )),
+                Container(
+                  margin: EdgeInsets.only(
+                      bottom: 10.0, left: 70.0, right: 70.0, top: 30),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      textColor: Colors.white,
+                      color: Colors.redAccent,
+                      splashColor: Colors.orangeAccent,
+                      child: Text(
+                        "SIGN UP",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      padding: EdgeInsets.all(20.0),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 50.0),
+                  child: Text("or sign up with")),
+                Container(
+                  margin: EdgeInsets.only(
+                      left: 75.0, right: 20.0, bottom: 10),
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          MyFlutterApp.twitter_circled_1,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                      new IconButton(
+                        onPressed: () {},
+                        icon: new Icon(
+                          FontAwesomeIcons.google,
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          MyFlutterApp.facebook_squared,
+                          color: Colors.blueAccent,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          ]
+        ),
+      );
+  }
+}
